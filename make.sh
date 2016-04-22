816458033464e7e518b39d3667377e4f3ab6b580
Jess Robertson <jesse.robertson@csiro.au>
2016-04-22 11:43:37 +0000

    Remove goxc command

--- make.sh
+++ make.sh
@@ -12,5 +12,4 @@ echo "Building docker image for docker-ssh-exec..."
 docker build --no-cache=true --tag rosetta/docker-ssh-exec .
 rm -f pkg/docker-ssh-exec
 
-echo "Done. To make a release, run: goxc"
 exit 0
